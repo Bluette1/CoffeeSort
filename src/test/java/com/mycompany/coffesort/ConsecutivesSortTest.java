4b26075076acfcfa0848ef3031ecffa3d79547ce
Bluette1 <marylene@gmail.com>
2020-01-09 09:59:26 +0200

    Fixes after review...
    Fixes after review...
    - added general sort class and test
    
    issue #2

--- src/test/java/com/mycompany/coffesort/ConsecutivesSortTest.java
+++ src/test/java/com/mycompany/coffesort/ConsecutivesSortTest.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -13,39 +13,41 @@ import org.junit.Test;
  * @author marylene
  * @since 9 January 2020
  */
-public class SortingConsecutivesTest {
+public class ConsecutivesSortTest {
 
     @Test
     /**
      * This will test that the given array is sorted correctly using insertion
      * sort by shifting the elements
      *
-     * @covers package com.mycompany.coffesort.SortingConsecutives.minimumSwaps()
+     * @covers com.mycompany.coffesort.ConsecutivesSort.doSort()
      */
-    public void testMinimumSwaps() {
-        System.out.println("minimumSwaps");
+    public void testDoSort() {
+        System.out.println("doSort");
 
         List<int[][]> testScenarios = new ArrayList(); //a list of test scenarios
 
-        testScenarios.add(new int[][]{{}, {0}});// empty
-        testScenarios.add(new int[][]{{1}, {0}});// one element
-        testScenarios.add(new int[][]{{1, 2}, {0}});// two elements; sorted
-        testScenarios.add(new int[][]{{3, 1, 2}, {2}});// three elements
+        testScenarios.add(new int[][]{{}, {}});// empty
+        testScenarios.add(new int[][]{{1}, {1}});// one element
+        testScenarios.add(new int[][]{{1, 2}, {1, 2}});// two elements; sorted
+        testScenarios.add(new int[][]{{3, 1, 2}, {1, 2, 3}});// three elements
         // simple test
-        testScenarios.add(new int[][]{{2, 3, 4, 1, 5}, {3}});
+        testScenarios.add(new int[][]{{2, 3, 4, 1, 5}, {1, 2, 3, 4, 5}});
 
         // another simple test
-        testScenarios.add(new int[][]{{4, 3, 1, 2}, {3}});
+        testScenarios.add(new int[][]{{4, 3, 1, 2}, {1, 2, 3, 4}});
+        
         // another simple test
-        testScenarios.add(new int[][]{{7, 1, 3, 2, 4, 5, 6}, {5}});
+        testScenarios.add(new int[][]{{7, 1, 3, 2, 4, 5, 6}, {1, 2, 3, 4, 5, 6, 7}});
 
         // Random tests
         for(int i = 0; i < 10; i++) {
            
-            int[] randArray = shuffle();
+            int[] randArray = createRandArray();
             // Create 'deepCopy' of array
             int[] testArray = Arrays.copyOf(randArray, randArray.length);
-            testScenarios.add(new int[][]{randArray,{minimumSwaps(testArray)}});
+            shuffle(testArray);
+            testScenarios.add(new int[][]{testArray, randArray});
         }
         
         Iterator<int[][]> scenarios = testScenarios.iterator();
@@ -53,25 +55,34 @@ public class SortingConsecutivesTest {
         while (scenarios.hasNext()) {
             int[][] scenario = scenarios.next();
 
-            int expResult = scenario[1][0];
+            int[] expResult = scenario[1];
             int[] array = scenario[0];
+            ConsecutivesSort.doSort(array);
 
-            assertEquals(
+            Assert.assertArrayEquals(
                     "The consecutive sorting result was expected to equal the expected result",
                     expResult,
-                    SortingConsecutives.minimumSwaps(array)
+                    array
             );
         }
     }
-
-    int[] shuffle() {
-        
-        int n = (int)(Math.random() * ((100 - 0) + 1));
+    
+    int[] createRandArray() {
+    
+         int n = (int)(Math.random() * ((100 - 0) + 1));
         // First create an array with consecutive values
         int[] result = new int[n];
         for (int i = 0; i < n; i++) {
             result[i] = i + 1;
         }
+        
+        return result;
+    
+    }
+
+    void shuffle(int[] array) {
+        
+        int n = array.length;
         
         // Shuffle the values in the array
         int count = 0;
@@ -81,33 +92,10 @@ public class SortingConsecutivesTest {
             int idx2 = 0 + (int)(Math.random() * ((n-1 - 0) + 1));
             
             // Swap the elements at indices 1 and 2
-            int temp = result[idx1];
-            result[idx1] = result[idx2];
-            result[idx2] = temp;
+            int temp = array[idx1];
+            array[idx1] = array[idx2];
+            array[idx2] = temp;
             count++;
         }
-        
-        return result;
-
-    }
-    
-    int minimumSwaps(int[] array) {
-        int n = array.length;
-        int minSwaps = 0;
-        
-       for (int i = 0; i < array.length; i++) {
-           // Loop through array putting values in the rightful places by swapping
-           while (array[i] != i + 1){// do the swap
-               int temp = array[array[i] - 1];
-           
-               array[array[i] - 1] = array[i];
-               array[i] = temp;
-               minSwaps++;
-           };
-           
-       }
-
-        return minSwaps;
-
     }
 }
